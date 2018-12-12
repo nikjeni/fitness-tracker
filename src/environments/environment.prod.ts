418c0b08c83343b900b4c5c9cfda6bac22264051
nikgavali <gavalinikhil700@gmail.com>
2018-12-13 00:19:21 +0530

    fixed issue

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyARDjderLTh9C3fub3TRD8bXIZUqL2wvvE",
+    authDomain: "nikat-fiteness-tracker.firebaseapp.com",
+    databaseURL: "https://nikat-fiteness-tracker.firebaseio.com",
+    projectId: "nikat-fiteness-tracker",
+    storageBucket: "nikat-fiteness-tracker.appspot.com",
+    messagingSenderId: "196043335725"
+  }
 };
